8a7d4ac63f73dd8b355856f7db754677f1496931
pahasara wickramasinghe <138836186+pahasara2003@users.noreply.github.com>
2023-12-18 05:38:29 +0530

    first

---
+++ serviceworker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "version-1";
+const urlsToCash = ["index.html", "offline.html"];
+
+const self = this;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("open cache");
+      cache.addAll(urlsToCash);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(async () => {
+        return fetch(event.request).catch(() => {
+          caches.match("offline.html");
+        });
+      })
+      .catch(() => {
+        caches.match("offline.html");
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
